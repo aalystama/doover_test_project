aeb45e001c5217886c8f12ed2b73ce1787c3a8dd
Timur Kim <allrighty1809@gmail.com>
2021-01-19 23:05:02 +0600

    Posts module. Done repo.

--- lib/features/posts/data/repositories/posts_repository.dart
+++ lib/features/posts/data/repositories/posts_repository.dart
@@ -1,8 +1,32 @@
+import 'package:doover_test_project/features/posts/data/datasources/posts_remote_datasource.dart';
 import 'package:doover_test_project/features/posts/data/models/comment.dart';
 import 'package:doover_test_project/features/posts/data/models/post.dart';
+import 'package:http/http.dart';
 
 abstract class PostsRepostitory {
-  List<Comment> getComments();
+  Future<List<Comment>> getComments(int postId);
 
-  List<Post> getPosts();
+  Future<List<Post>> getPosts();
+}
+
+class PostsRepositoryImpl implements PostsRepostitory {
+  final PostsRemoteDataSource postsRemoteDataSource;
+
+  PostsRepositoryImpl(this.postsRemoteDataSource);
+
+  @override
+  Future<List<Comment>> getComments(int postId) async {
+    Response data = await postsRemoteDataSource.getComments(postId);
+
+    return (data.body as List)
+        .map((object) => Comment.fromJson(object))
+        .toList();
+  }
+
+  @override
+  Future<List<Post>> getPosts() async {
+    Response data = await postsRemoteDataSource.getPosts();
+
+    return (data.body as List).map((object) => Post.fromJson(object)).toList();
+  }
 }
