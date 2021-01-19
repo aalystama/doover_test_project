bf4b73850941738ce9c86ee014b5ec1a13cc29ac
Timur Kim <allrighty1809@gmail.com>
2021-01-19 17:58:39 +0600

    Project setup. Added color const.

---
+++ lib/core/consts/colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class DooverColors {
+  static const Color kScaffoldBackgroundColor = Color(0xFF0F0B21);
+
+  static const Color kCardBackgroundColor = Color(0xFF221C44);
+
+  static const Color kBottomNavigationBarItemColor = Color(0xFF59527F);
+
+  static const Color kInactiveCheckboxColor = Color(0xFF7F78A7);
+
+  static const Color kActiveCheckboxTile = Color.fromRGBO(108, 99, 255, 0.25);
+
+  static const Color kButtonColor = Color(0xFF6C63FF);
+}
