1d64ba4602e96db28c137f220e484ea7a149f2df
Timur Kim <allrighty1809@gmail.com>
2021-01-19 17:27:16 +0600

    Project setup. Finished adding text styles.

--- lib/core/consts/text_style.dart
+++ lib/core/consts/text_style.dart
@@ -2,6 +2,18 @@ import 'package:doover_test_project/core/consts/font_weight.dart';
 import 'package:flutter/material.dart';
 
 class DooverTextStyle {
+  static const TextStyle kCommentAuthorTextStyle = kDetailedPostTitleTextStyle;
+
+  static const TextStyle kCommentBodyTextStyle = kPostBodyTextStyle;
+
+  static const TextStyle kPhotoTitleTextStyle = kAlbumDescriptionTextStyle;
+
+  static const TextStyle kTodoTextStyle = kPostTitleTextStyle;
+
+  static const TextStyle kContactFullNameTextStyle = kPostTitleTextStyle;
+
+  static const TextStyle kContactCredentialBodyTextStyle = kPostTitleTextStyle;
+
   static const TextStyle kAppBarTextStyle = TextStyle(
     color: Colors.white,
     fontSize: 20,
@@ -32,9 +44,27 @@ class DooverTextStyle {
     fontWeight: DooverFontWeight.kMediumFontWeight,
   );
 
-  static const TextStyle kCommentAuthorTextStyle = kDetailedPostTitleTextStyle;
+  static const TextStyle kAlbumTitleTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontWeight: DooverFontWeight.kMediumFontWeight,
+  );
 
-  static const TextStyle kCommentBodyTextStyle = kPostBodyTextStyle;
+  static const TextStyle kAlbumDescriptionTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 12,
+    fontWeight: DooverFontWeight.kMediumFontWeight,
+  );
 
-// TODO: Добавить стили текста всех модулей, помимо модуля постов
+  static const TextStyle kContactCredentialTitleTextStyle = TextStyle(
+    color: Color(0xFF7F78A7), // TODO: Create colors consts
+    fontSize: 12,
+    fontWeight: DooverFontWeight.kMediumFontWeight,
+  );
+
+  static const TextStyle kContactNickTextStyle = TextStyle(
+    color: Color(0xFF6C63FF),
+    fontSize: 12,
+    fontWeight: DooverFontWeight.kMediumFontWeight,
+  );
 }
